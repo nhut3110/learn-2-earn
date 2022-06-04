81d9d09e57841a91c8bcbda55b299e9281dc6f25
Lee.C32 <90383072+CuongXuanLe@users.noreply.github.com>
2022-06-04 17:42:40 +0700

    add MarketNAv

---
+++ components/Market/MarketNavigation/MarketNav.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Ionicons, MaterialIcons } from "@expo/vector-icons";
+import {
+        Box,
+        Heading,  
+        Image, 
+        Text, 
+        Center, 
+        HStack, 
+        NativeBaseProvider, 
+        Button, 
+        VStack, 
+        ScrollView, 
+        SearchIcon,
+        Input,
+        Icon,
+        useDisclose,
+        Actionsheet,
+        FavouriteIcon,
+        } from "native-base";
